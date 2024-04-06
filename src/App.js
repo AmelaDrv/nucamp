7a1983a90a8a8605d38359c6466eb18978fdf7de
AD <45442527+AmelaDrv@users.noreply.github.com>
2024-04-06 12:20:29 -0400

    added files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
 import './App.css';
-import CampsiteList from './features/campsites/CampsiteList.js';
+import CampsiteList from './features/campsites/CampsiteList';
+import Header from './components/Header';
 import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div className='App'>
-      <Navbar dark color='primary' sticky='top' expand='md'>
-        <Container>
-          <NavbarBrand href='/'>
-            <img src={NucampLogo} alt='nucamp logo' />
-          </NavbarBrand>
-        </Container>
-      </Navbar>
-      <CampsiteList/>
-      <Footer/>
+    <div className="App">
+      <Header />
+      <CampsiteList />
+      <Footer />
     </div>
   );
-  }
+}
 
 export default App;
