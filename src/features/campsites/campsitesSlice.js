e808eaa887be7c19ca05985f9f56091d75c71f45
AD <45442527+AmelaDrv@users.noreply.github.com>
2024-04-08 22:25:02 -0400

    Added image click so image campsite slice changes based on where we click

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -4,7 +4,11 @@ export const selectAllCampsites = () => {
     return CAMPSITES;
 };
 
-export const selectRandomCampsite = () => {
-    const random = Math.floor(Math.random() * CAMPSITES.length)
-    return CAMPSITES[random];
+// export const selectRandomCampsite = () => {
+//     const random = Math.floor(Math.random() * CAMPSITES.length)
+//     return CAMPSITES[random];
+// };
+
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find((campsite) => campsite.id === id);
 };
