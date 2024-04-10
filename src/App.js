009fa5cc459d8a6515bb3a2b8a4d184eb1879d5d
AD <45442527+AmelaDrv@users.noreply.github.com>
2024-04-09 20:36:18 -0400

    added routing and hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import HomePage from './pages/HomePage';
+import ContactPage from './pages/ContactPage';
 import './App.css';
 
 
@@ -9,7 +12,11 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesDirectoryPage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+      </Routes>
       <Footer />
     </div>
   );
